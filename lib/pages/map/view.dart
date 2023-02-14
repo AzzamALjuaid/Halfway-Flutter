1cc80a5f2324a7f4b1492c44410567bfbc8ac4e6
AzzamALjuaid <92253516+AzzamALjuaid@users.noreply.github.com>
2023-02-14 22:27:43 +0300

    Setup map page ✓

--- lib/pages/map/view.dart
+++ lib/pages/map/view.dart
@@ -10,13 +10,27 @@ import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 
+import '../../common/widgets/app.dart';
+
 class MapPage extends GetView<MapController> {
   const MapPage({Key? key}) : super(key: key);
+
+  AppBar _buildAppbar() {
+    return transparentAppBar(
+        title: Text(
+          "Map",
+          style: TextStyle(
+              color: AppColors.primaryBackground,
+              fontSize: 18.sp,
+              fontWeight: FontWeight.w600),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
+      appBar: _buildAppbar(),
         body: Center(
       child: Column(
         children: [
